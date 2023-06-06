e72bf5f022b51e0732b8d95d6a1958e5bee00263
Ikhor03 <imamkhoiri725@gmail.com>
2023-06-06 12:05:49 +0700

    debug cors

--- app/cart/constroller.js
+++ app/cart/constroller.js
@@ -31,7 +31,7 @@ const update = async (req, res, next) => {
                 }
             }
         }))
-
+        res.header("Access-Control-Allow-Origin", "*")
         res.status(200).send({message: 'Cart updated Successfully', data: cartItems})
 
     } catch (err) {
@@ -49,6 +49,7 @@ const update = async (req, res, next) => {
 const index = async (req, res, next) => {
     try {
         let items = await CartItem.find({ user: req.user._id }).populate('product')
+        res.header("Access-Control-Allow-Origin", "*");
         res.json(items)
     } catch (err) {
         if(err && err.name === 'ValidationError') {

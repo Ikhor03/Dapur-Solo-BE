1e91c3519b1dc9c0da04811a74d0918546d83f0f
Ikhor03 <imamkhoiri725@gmail.com>
2023-06-07 12:31:33 +0700

    add cors handling

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(cors())
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));

637303be376063841470f73dc33d54d04e71d48e
Ikhor03 <imamkhoiri725@gmail.com>
2023-06-07 13:42:09 +0700

    add cors handling

--- app/cart/router.js
+++ app/cart/router.js
@@ -3,6 +3,7 @@ const { police_check } = require('../../middlewares')
 const controller = require('./constroller')
 
 router.put('/cart', police_check('update', 'Cart'), controller.update)
+router.post('/cart', police_check('update', 'Cart'), controller.update)
 router.get('/cart', police_check('read', 'Cart'), controller.index)
 
 module.exports = router
